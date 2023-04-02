dcef3b9689fe72a830a20293986b09cd4bf9e56b
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-02 20:14:41 +0200

    Added calling of getClients action inside ClientList

---
+++ src/components/ClientList.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div class="d-flex mb-1 row px-2 px-lg-3">
+      <p class="mb-0 d-none d-lg-block col-lg-4">Nom usuari</p>
+      <p class="mb-0 d-none d-lg-block col-lg-3">Email</p>
+      <p class="mb-0 d-none d-lg-block col-lg-3">Identificació</p>
+      <p class="mb-0 d-none d-lg-block col-lg-2">Telèfon</p>
+    </div>
+    <div v-for="(client, index) of clients" :key="index" class="mb-1">
+      {{ client }}
+    </div>
+  </section>
+</template>
+
+<script>
+import { useClientStore } from '../stores/ClientStore'
+
+export default {
+  setup() {
+    const clientStore = useClientStore()
+    return { clientStore }
+  },
+  computed: {
+    clients() {
+      return this.clientStore.clients
+    }
+  },
+  created() {
+    this.getClients()
+  },
+  methods: {
+    async getClients() {
+      await this.clientStore.getClients()
+    }
+  }
+}
+</script>
