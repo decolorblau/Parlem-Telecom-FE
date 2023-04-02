0aa5188f6a9f650f19ec21c86d7493f6844e43a4
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-02 13:48:09 +0200

    Cleaned Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <div class="w-100 mt-3 d-flex flex-column align-items-center">
+    <h1 class="mb-3">Pagina d'inici</h1>
+    <RouterLink to="/clients">Començar a explorar</RouterLink>
+  </div>
+</template>
