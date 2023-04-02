9931b8d475bebfd01a0168260ee58c4b28655101
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-02 20:15:16 +0200

    Added clientList to ClientsView

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="">
-    <RouterLink to="/">&lt; Back</RouterLink>
-    <h1>Pagina dels clients</h1>
+  <div class="p-4 p-lg-5 fullPage">
+    <RouterLink to="/"><p class="small">&lt; Back</p></RouterLink>
+    <div class="d-flex mt-4 mb-2">
+      <h4>Clients</h4>
+    </div>
+    <ClientList />
   </div>
 </template>
 
-<style></style>
+<script>
+import ClientList from '../components/ClientList.vue'
+
+export default {
+  name: 'ClientsView',
+  components: {
+    ClientList
+  }
+}
+</script>
+
+<style>
+.fullPage {
+  min-height: calc(100vh - 80px);
+  background-color: rgb(245, 245, 245);
+}
+</style>
