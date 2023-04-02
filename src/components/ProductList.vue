0068969df3fd39bb1cd4e60ef6b8635b7bb8e670
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-03 00:19:40 +0200

    Added productListItem to Product list component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,23 @@
 <template>
   <section>
-    {{ products }}
+    <div class="d-flex w-100 mt-4 mb-2 justify-content-between">
+      <h4>Productes contractats ({{ products.length }})</h4>
+    </div>
+    <div class="d-flex mb-1 row px-2 px-lg-3">
+      <p class="mb-0 d-none d-lg-block col-lg-4">Nom</p>
+      <p class="mb-0 d-none d-lg-block col-lg-3">Tipus</p>
+      <p class="mb-0 d-none d-lg-block col-lg-3">Número terminal</p>
+      <p class="mb-0 d-none d-lg-block col-lg-2">Data venta</p>
+    </div>
+    <div v-for="(product, index) of products" :key="index" class="mb-1">
+      <ProductListItem :product="product" />
+    </div>
   </section>
 </template>
 
 <script>
 import { useProductStore } from '../stores/ProductsStore'
+import ProductListItem from './ProductListItem.vue'
 
 export default {
   name: 'ProductList',
@@ -13,6 +25,7 @@ export default {
     const productStore = useProductStore()
     return { productStore }
   },
+  components: { ProductListItem },
   computed: {
     id() {
       return this.$route.params.id
