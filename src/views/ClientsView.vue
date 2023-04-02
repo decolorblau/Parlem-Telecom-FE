fe6407b0d946bf6e333ab38e4843b5da10fe7499
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-02 20:44:57 +0200

    Deleted back route

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-4 p-lg-5 fullPage">
-    <RouterLink to="/"><p class="small">&lt; Back</p></RouterLink>
     <div class="d-flex mt-4 mb-2">
       <h4>Clients</h4>
     </div>
