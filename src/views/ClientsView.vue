b59279190f646f0f19a3a191fd5844ea635ba180
Claudia Font Marques <claudiafontmarques@gmail.com>
2023-04-02 13:51:43 +0200

    Cleaned styles of ClientsView

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="about">
+  <div class="">
     <RouterLink to="/">&lt; Back</RouterLink>
     <h1>Pagina dels clients</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
